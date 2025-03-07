71641313f898ab5a1b984270f6d4a77e0080ba2d
Akshada0612 <155293231+Akshada0612@users.noreply.github.com>
2025-03-07 21:27:24 +0530

    Add files via upload

---
+++ DuplicateInArray.java
@@ -0,0 +1,19 @@
+class  DuplicateInArray
+{
+	public static void main(String[] args) 
+	{
+		int arr[]={2,4,5,2,6};
+		int dup=arr[0];
+		
+		for (int i=1;i<arr.length;i++ )
+		{
+			if(arr[0]==i)
+			{
+				System.out.print("Duplicate elements of this array are:"+arr[0]);
+			}else
+			{
+				
+			}
+		}
+	}
+}
